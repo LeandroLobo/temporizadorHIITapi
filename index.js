2c1237e8f498957069a71dd6ac13196b566382fe
LeandroLobo <leandrolobomarquez@gmail.com>
2021-05-06 16:53:35 -0300

    base de datos mongoDB lista, inicializa la colección UsuarioDefault

--- index.js
+++ index.js
@@ -2,19 +2,61 @@ import express from 'express';
 import mongoose from 'mongoose';
 import model from './lib/models.js';
 // import sendEmail from './lib/mailer.js';
-// import initEmail from './lib/initEmail.js';
-// import validateEmail from './lib/validateEmail.js';
 
 const app = express();
+const defaultUser = {
+    name: 'UsuarioDefault',
+    sessions: [
+        {
+          name: 'Sesión Standard',
+          duration: 480,
+          id: '0000',
+          sets: [
+            {
+              name: '45/15',
+              work: 45,
+              rest: 15,
+              reps: 8
+            }
+          ]
+        },
+        {
+          name: 'Sesión Standard Heavy',
+          duration: 960,
+          id: '0001',
+          sets: [
+            {
+              name: '90/30',
+              work: 90,
+              rest: 30,
+              reps: 8
+            }
+          ]
+        },
+        {
+          name: 'Test',
+          duration: 26,
+          id: '0002',
+          sets: [
+            {
+              name: 'anything',
+              work: 8,
+              rest: 5,
+              reps: 2
+            }
+          ]
+        }
+    ]
+}
 
 app.use(express.urlencoded({extended: true}));
 
 app.get('/getuser', async (req,res) => {
-    const query = {_id: req.query.id}
-    const user = await model.user.find(query, (err) => {
+    const query = {name: req.query.name}
+    const userData = await model.userData.find(query, (err) => {
         if(err) throw new Error(`Reading error: ${err}`);
     }).lean();
-    console.log(user);
+    res.send(userData);
 });
 
 /******************************************
@@ -25,7 +67,7 @@ app.set('PORT', process.env.PORT || 8888);
 mongoose.connect('mongodb://localhost/temp-hiit', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, err => {
+}, async err => {
     if(err) throw new Error(`Connection error in database: ${err}`);
     //=> Serving app
     console.log('Database connected');
@@ -33,7 +75,14 @@ mongoose.connect('mongodb://localhost/temp-hiit', {
         console.log('\n-------------------------------------');
         console.log(`Server ready, listening at PORT: ${server.address().port}`);
         console.log('-------------------------------------\n');
-        initEmail();
     });
     server.on('error', error => console.log(`Server error => ${error}`));
+
+    //////////////////////////////////////////////////////////
+    const newUser = new model.userData(defaultUser);
+    newUser.save(async err => {
+        if(err) throw new Error(`Writing error: ${err}`);
+        console.log('New product added to database\n');
+    });
+    //////////////////////////////////////////////////////////
 });
